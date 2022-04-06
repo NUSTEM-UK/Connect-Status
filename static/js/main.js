cf1c0d50aafcf7dff6317c3f2d9caac2c1af3a1b
jjsanderson <jonathan@quernstone.com>
2022-04-06 13:53:17 +0100

    Basic device table, with refresh.

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(() => {
     // Initialize page
     updateMoods();
-    setInterval(updateMoods, 5000);
+    setInterval(updateMoods, 10000);
+    updateDevices();
+    setInterval(updateDevices, 5000);
+    // $('#mac-table').DataTable();
 });
 
 function updateMoods(e) {
@@ -14,15 +17,45 @@ function parseMoods(data) {
     // e.preventDefault();
     console.log(data);
 
-    const $showMoods = $('#show-moods');
-    const $raw = $('pre');
+    // Grab the selector
+    const showMoods = $('#show-moods');
+
     // Parse mood JSON data, insert into list items
-    console.log(data.mood_name);
-
-    $('#show-moods').html('<ul>');
+    showMoods.html('<ul>');
     var moodsInfo = data.map( function(item) {
-        $('#show-moods').append('<li>' + item.mood_name + ' : ' + item.mood_count + '</li>');
+        showMoods.append('<li>' + item.mood_name + ' : ' + item.mood_count + '</li>\n');
     });
-    $('#show-moods').append('</ul>');
+    showMoods.append('</ul>');
 
+}
+
+function updateDevices(e) {
+    $.getJSON('/getDevices.php', (data) => {
+        parseDevices(data);
+    });
+};
+
+function parseDevices(data) {
+    console.log(data);
+
+    // Grab the selector
+    const deviceTable = $('#mac-table');
+    deviceTable.html('<thead>\n<tr>\n'
+                      + '<th scope="col">ID</th>\n'
+                      + '<th scope="col">Last Seen</th>\n'
+                      + '<th scope="col">Pings</th>\n'
+                      + '<th scope="col">Cohort</th>\n'
+                      + '<th scope="col">Label</th>\n'
+                      + '</tr>\n</thead>\n<tbody>\n');
+    var devicesInfo = data.map( function(item) {
+        // deviceTable.append('<tr>\n');
+        deviceTable.append('<tr>\n<td scope="row">' + item.mac_address + '</td>\n'
+                            + '<td>' + item.last_seen + '</td>\n'
+                            + '<td>' + item.pings + '</td>\n'
+                            + '<td>' + item.cohort + '</td>\n'
+                            + '<td>' + item.label + '</td>\n</tr>\n'
+        );
+        // deviceTable.append('</tr>\n');
+    });
+    deviceTable.append('</tbody>\n');
 }
