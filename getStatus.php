19ad60a0a6f1b434141a4dffb3eea1472e3d1a63
jjsanderson <jonathan@quernstone.com>
2022-04-06 08:54:47 +0100

    JSON data exports

--- getStatus.php
+++ getStatus.php
@@ -3,21 +3,23 @@
 try {
     require_once('functions.php');
     $dbConn = getConnection();
-    echo getMoodData($dbConn);
+    echo getStatusData($dbConn);
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
 
-function getMoodData($dbConn) {
+function getStatusData($dbConn) {
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Origin: *");
 
-    $sql = "SELECT mood_name, mood_count FROM moods";
+    $sql = "SELECT mac_address, last_seen, pings, label, cohort
+            FROM devices
+            WHERE first_seen IS NOT NULL";
 
     try {
-        $moodResults = $dbConn->query($sql);
-        $moodData = $moodResults->fetchAll(PDO::FETCH_ASSOC);
-        return json_encode($moodData);
+        $statusResults = $dbConn->query($sql);
+        $statusData = $statusResults->fetchAll(PDO::FETCH_ASSOC);
+        return json_encode($statusData);
     } catch (Exception $e) {
         throw new Exception("Database problem: " . $e->getMessage(), 0, $e);
     }
